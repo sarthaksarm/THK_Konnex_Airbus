19bccf9214922c3b415915013608395040f999fb
sarthaksarm <sarmsarthak@gmail.com>
2021-05-20 02:19:11 +0530

    Homepage widgets

--- Konnex/app/src/main/java/com/sarm/konnex/MainActivity.java
+++ Konnex/app/src/main/java/com/sarm/konnex/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -20,34 +21,36 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     Intent i=new Intent(MainActivity.this,NavSearch.class);
+                    Toast.makeText(MainActivity.this, "Switching to Application Navigation page", Toast.LENGTH_LONG).show();
                     startActivity(i);
                     return true;
-                case R.id.navigation_voice:
-                    //voice();
-                    return true;
+//                case R.id.navigation_voice:
+//                    //voice();
+//                    return true;
 
                 case R.id.navigation_chatbot:
-                    Intent i1=new Intent(MainActivity.this,MapsActivity.class);
+                    Intent i1=new Intent(MainActivity.this,ChatBot.class);
+                    Toast.makeText(MainActivity.this, "Contacting Chat-bot support", Toast.LENGTH_LONG).show();
                     startActivity(i1);
                     return true;
 
                 case R.id.navigation_feedback:
                     Intent i2=new Intent(MainActivity.this,ReportBug.class);
+                    Toast.makeText(MainActivity.this, "Opening a 'Report Bug' case", Toast.LENGTH_LONG).show();
                     startActivity(i2);
                     return true;
 
                 case R.id.navigation_announce:
                     Intent i3=new Intent(MainActivity.this,Announcements.class);
+                    Toast.makeText(MainActivity.this, "Checking Collective Announcements section", Toast.LENGTH_LONG).show();
                     startActivity(i3);
                     return true;
 
                 case R.id.navigation_performance:
                     Intent i4=new Intent(MainActivity.this,Performance.class);
+                    Toast.makeText(MainActivity.this, "Measuring application usage and performance", Toast.LENGTH_LONG).show();
                     startActivity(i4);
                     return true;
-
-
-
             }
             return false;
         }
